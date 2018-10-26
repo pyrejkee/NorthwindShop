62e768e46b47c5d473fdb18c8ad1eb07c61d03cb
Kiryl_Kiryanchykau <kiryl_kiryanchykau@epam.com>
2018-10-26 17:23:55 +0300

    Add unit tests for category controller

---
+++ ControllerTests/CategoryControllerTests.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Moq;
+using NorthwindShop.BLL.EntitiesDTO;
+using NorthwindShop.BLL.Services.Interfaces;
+using NorthwindShop.Web.Automapper;
+using NorthwindShop.Web.Controllers;
+using NorthwindShop.Web.ViewModels;
+using Xunit;
+
+namespace ControllerTests
+{
+    public class CategoryControllerTests
+    {
+        [Fact]
+        public void Index_WhenThereAreExistCategories_ShouldReturnViewWithCategories()
+        {
+            // Arrange
+            var configMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new WebProfile());
+            });
+            var autoMapper = configMapper.CreateMapper();
+            var categoryService = new Mock<ICategoryService>();
+            var distributedCache = new Mock<IDistributedCache>();
+            categoryService.Setup(x => x.Get()).Returns(new List<CategoryDTO>
+            {
+                new CategoryDTO {Name = "Category 1", Description = "Description 1"},
+                new CategoryDTO {Name = "Category 2", Description = "Description 2"}
+            });
+            var controller = new CategoryController(categoryService.Object, autoMapper, distributedCache.Object);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<List<CategoryViewModel>>(((ViewResult) result).ViewData.Model);
+            IEnumerable<CategoryViewModel> expectedCategoryViewModels = new List<CategoryViewModel>
+            {
+                new CategoryViewModel { Name = "Category 1", Description = "Description 1"},
+                new CategoryViewModel { Name = "Category 2", Description = "Description 2"}
+            };
+            Assert.Equal(expectedCategoryViewModels, model.AsEnumerable(), new ValueComparer<CategoryViewModel>());
+        }
+
+        [Fact]
+        public void Image_ShouldReturnFileContentResult()
+        {
+            // Arrange
+            var configMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new WebProfile());
+            });
+            var autoMapper = configMapper.CreateMapper();
+            var categoryService = new Mock<ICategoryService>();
+            var distributedCache = new Mock<IDistributedCache>();
+            var controller = new CategoryController(categoryService.Object, autoMapper, distributedCache.Object);
+
+            // Act
+            var result = controller.Image(1);
+
+            // Assert
+            Assert.IsType<FileContentResult>(result);
+        }
+
+        [Fact]
+        public void EditCategory_GET_WhenSpecifiedCategoryWasNotFound_ShouldReturnRedirectToIndex()
+        {
+            // Arrange
+            var configMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new WebProfile());
+            });
+            var autoMapper = configMapper.CreateMapper();
+            var categoryService = new Mock<ICategoryService>();
+            var distributedCache = new Mock<IDistributedCache>();
+            categoryService.Setup(x => x.Get()).Returns(new List<CategoryDTO>
+            {
+                new CategoryDTO {Name = "Category 1", Description = "Description 1"},
+                new CategoryDTO {Name = "Category 2", Description = "Description 2"}
+            });
+            var controller = new CategoryController(categoryService.Object, autoMapper, distributedCache.Object);
+            var categoryId = 3;
+
+            // Act
+            var result = controller.EditCategory(categoryId);
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectToActionResult.ActionName);
+        }
+    }
+}
---
+++ ControllerTests/ValueComparer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ControllerTests
+{
+    public class ValueComparer<T> : IEqualityComparer<T>
+    {
+        public string[] ExcludeProperties { get; set; }
+
+        public bool Equals(T x, T y)
+        {
+            if (ReferenceEquals(x, y))
+            {
+                return true;
+            }
+
+            if (object.Equals(x, default(T)) || object.Equals(y, default(T)) || (x.GetType() != y.GetType()))
+            {
+                return false;
+            }
+
+            var properties = x.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance).ToList();
+            if (!properties.Any())
+            {
+                throw new InvalidOperationException("Objects don't have properties to compare");
+            }
+
+            if (ExcludeProperties?.Any() == true)
+            {
+                properties = properties.Where(p => !ExcludeProperties.Contains(p.Name)).ToList();
+            }
+
+            foreach (var property in properties)
+            {
+                var valueX = property.GetValue(x);
+                var valueY = property.GetValue(y);
+
+                if (!Equals(valueX, valueY))
+                {
+                    //TestContext.Progress.Write($"Check for {property.Name} is failed: {valueX} is not equal to {valueY}");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public int GetHashCode(T obj)
+        {
+            var properties = obj.GetType().GetProperties();
+            unchecked
+            {
+                return properties.Aggregate(17, (current, property) => current * property.GetValue(obj).GetHashCode() * 31);
+            }
+        }
+    }
+}
