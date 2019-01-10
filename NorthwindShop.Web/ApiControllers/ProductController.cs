a97290467b52ae61c99416a99647fb1fdeb06567
Kiryl_Kiryanchykau <kiryl_kiryanchykau@epam.com>
2019-01-10 12:17:44 +0300

    Add web api controllers for showing list of products and categories

---
+++ NorthwindShop.Web/ApiControllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindShop.BLL.Services.Interfaces;
+using NorthwindShop.Web.ViewModels;
+
+namespace NorthwindShop.Web.ApiControllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductViewModel>>> GetAllProducts()
+        {
+            var productDtos = await _productService.Get();
+            var products = _mapper.Map<List<ProductViewModel>>(productDtos);
+
+            return products;
+        }
+    }
+}
