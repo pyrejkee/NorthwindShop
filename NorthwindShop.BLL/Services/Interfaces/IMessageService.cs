3a8b514c015c9f93942ed7904b5a22f6b9757e1c
Kiryl_Kiryanchykau <kiryl_kiryanchykau@epam.com>
2019-01-14 17:34:40 +0300

    Add message service for sending emails (for now to file)

---
+++ NorthwindShop.BLL/Services/Interfaces/IMessageService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace NorthwindShop.BLL.Services.Interfaces
+{
+    public interface IMessageService
+    {
+        Task Send(string email, string subject, string message);
+    }
+}
